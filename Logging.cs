0c017be6c7ac3df8f904c26094569ccb8f910cce
Niko2405 <nilskornmann@gmail.com>
2023-12-23 16:14:12 +0100

    Project created

---
+++ Logging.cs
@@ -0,0 +1,46 @@
+﻿namespace SCI_Logger
+{
+	public class Logging
+	{
+		public static bool IsDebugEnabled = false;
+		public static void Info(string message)
+		{
+			ConsoleColor currentForegroundColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Green;
+
+			Console.WriteLine($"[Info]\t\t{message}");
+
+			Console.ForegroundColor = currentForegroundColor;
+		}
+		public static void Error(string message)
+		{
+			ConsoleColor currentForegroundColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Red;
+
+			Console.WriteLine($"[Error]\t\t{message}");
+
+			Console.ForegroundColor = currentForegroundColor;
+		}
+		public static void Warn(string message)
+		{
+			ConsoleColor currentForegroundColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			Console.WriteLine($"[Warning]\t{message}");
+
+			Console.ForegroundColor = currentForegroundColor;
+		}
+		public static void Debug(string message)
+		{
+			if (IsDebugEnabled)
+			{
+				ConsoleColor currentForegroundColor = Console.ForegroundColor;
+				Console.ForegroundColor = ConsoleColor.Cyan;
+
+				Console.WriteLine($"[Debug]\t\t{message}");
+
+				Console.ForegroundColor = currentForegroundColor;
+			}
+		}
+	}
+}
