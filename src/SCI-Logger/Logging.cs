ac8a35b929454379e191933adc150dce638a1f28
Niko2405 <nilskornmann@gmail.com>
2023-12-31 21:12:12 +0100

    Update Logging.cs
    Add Time and file writer

--- src/SCI-Logger/Logging.cs
+++ src/SCI-Logger/Logging.cs
@@ -3,32 +3,62 @@
 	public class Logging
 	{
 		public static bool IsDebugEnabled = false;
+
+		private static string GetCurrentTime()
+		{
+			return DateTime.Now.ToString("HH:mm");
+		}
+		private static void WriteInLogFile(string level, string message)
+		{
+			
+			string pathToLogFile = "./latest.log";
+			try
+			{
+				if (!File.Exists(pathToLogFile))
+				{
+					File.WriteAllText(pathToLogFile, $"[{level}:{GetCurrentTime()}]\t{message}\n");
+					return;
+				}
+				else
+				{
+					File.AppendAllText(pathToLogFile, $"[{level}:{GetCurrentTime()}]\t{message}\n");
+				}
+			}
+			catch (Exception ex)
+			{
+				Error(ex.Message);
+			}
+		}
+		
 		public static void Info(string message)
 		{
 			ConsoleColor currentForegroundColor = Console.ForegroundColor;
 			Console.ForegroundColor = ConsoleColor.Green;
 
-			Console.WriteLine($"[Info]\t\t{message}");
+			Console.WriteLine($"[Info:{GetCurrentTime()}]\t\t{message}");
 
 			Console.ForegroundColor = currentForegroundColor;
+			WriteInLogFile("Info", message);
 		}
 		public static void Error(string message)
 		{
 			ConsoleColor currentForegroundColor = Console.ForegroundColor;
 			Console.ForegroundColor = ConsoleColor.Red;
 
-			Console.WriteLine($"[Error]\t\t{message}");
+			Console.WriteLine($"[Error:{GetCurrentTime()}]\t\t{message}");
 
 			Console.ForegroundColor = currentForegroundColor;
+			WriteInLogFile("Error", message);
 		}
 		public static void Warn(string message)
 		{
 			ConsoleColor currentForegroundColor = Console.ForegroundColor;
 			Console.ForegroundColor = ConsoleColor.Yellow;
 
-			Console.WriteLine($"[Warning]\t{message}");
+			Console.WriteLine($"[Warning:{GetCurrentTime()}]\t\t{message}");
 
 			Console.ForegroundColor = currentForegroundColor;
+			WriteInLogFile("Warn", message);
 		}
 		public static void Debug(string message)
 		{
@@ -37,9 +67,10 @@
 				ConsoleColor currentForegroundColor = Console.ForegroundColor;
 				Console.ForegroundColor = ConsoleColor.Cyan;
 
-				Console.WriteLine($"[Debug]\t\t{message}");
+				Console.WriteLine($"[Debug:{GetCurrentTime()}]\t\t{message}");
 
 				Console.ForegroundColor = currentForegroundColor;
+				WriteInLogFile("Debug", message);
 			}
 		}
 		public static void PrintHeader(string title)
